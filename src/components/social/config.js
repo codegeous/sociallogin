af17ca35d7e8821c5d2b87060d2947be147f406e
Prem Kumar <premkumarssp@gmail.com>
2017-08-04 01:28:24 +0530

    Config file

---
+++ src/components/social/config.js
@@ -0,0 +1,5 @@
+export default {
+    facebook: "", //Facebook App Id
+    linkedin: "", //Linkedin app id
+    google  : "846832770923-nq1asqrnbgordka380aee3fu0th7kk7l.apps.googleusercontent.com" //Google client id  (7687687687-nq1asqrnbgordksqerer23dewqqe.apps.googleusercontent.com)
+}
